a59ad1c6dc6074300fe5dcbc7339924e97d5dad6
Vladimir Romanov <vladromweb@gmail.com>
2016-01-15 22:43:06 -0500

    Final Commit
    Final Commit

---
+++ SoilHumidity.ino
@@ -0,0 +1,18 @@
+int waterPump = 13;
+void setup() {
+  Serial.begin(9600);
+  pinMode(waterPump, OUTPUT);
+}
+
+void loop() {
+  int humidityRaw = analogRead(A0); // 1023 to 0 ===> 0 to 100%
+  int humidityReal = map(humidityRaw, 1023, 0, 0, 100);
+  Serial.println(humidityReal);
+  delay(100);
+  if (humidityReal < 90)
+  {
+    digitalWrite(waterPump, HIGH);
+  }else{
+    digitalWrite(waterPump, LOW);
+  }
+}
